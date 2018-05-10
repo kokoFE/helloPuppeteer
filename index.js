3dc80adcd315caad53d3b5287123c6cb13d95614
kokoFE <frontend_koko@yeah.net>
2018-05-10 10:07:34 +0800

    update index.js

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 const puppeteer = require('puppeteer');
+const CREDS = require('./creds');
+
+const USERNAME_SELECTOR = '#login_field';
+const PASSWORD_SELECTOR = '#password';
+const BUTTON_SELECTOR = '#login > form > div.auth-form-body.mt-3 > input.btn.btn-primary.btn-block';
 
 (async () => {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
-  await page.goto('https://baidu.com');
-  await page.screenshot({path: 'example.png'});
+  await page.goto('https://github.com/login');
+  // await page.click(USERNAME_SELECTOR);
+  await page.type(USERNAME_SELECTOR, CREDS.username);
+  await page.type(PASSWORD_SELECTOR, CREDS.password);
+  await page.click(BUTTON_SELECTOR);
+  
+  const userToSearch = 'john';
+  const searchUrl = `https://github.com/search?q=${userToSearch}&type=Users&utf8=%E2%9C%93`;
+  await page.goto(searchUrl);
+  await page.waitFor(2 * 1000);
+  // await page.screenshot({ path: 'screenshots/github.png'})
 
-  await browser.close();
+  // await browser.close();
 })();
